ad71a25321b1c4b3bcbe7adb10ffa0a9398e57ce
David Olowomeye <olowomeyedavid242@gmail.com>
2023-11-03 21:41:48 +0000

    Playing with the Turtle Coordinate System

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
 from turtle import Turtle, Screen
 
-tim = Turtle()
-tim.shape("turtle")
-tim.color("green")
-tim.forward(100)
-tim.right(90)
+screen = Screen()
+
+screen.setup(width=500, height=400)
+user_bet = screen.textinput(title="Kindly predict the race winner", prompt="Which turtle will winn the race? Enter a colour:")
+colours = ["red", "orange", "yellow", "blue", "purple", "green"]
+y_positions = [-70, -40, -10, 20, 50, 80]
+
+for turtle_index in range(0, 6):
+    tim = Turtle(shape="turtle")
+    tim.color(colours[turtle_index])
+    tim.penup()
+    tim.goto(x=-230, y=y_positions[turtle_index])
+
+screen.exitonclick()
+
+
 
 
